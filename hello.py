edec125637d1e90049d34b37322a469a67c70c3a
shehraj123 <shehrajsingh@gmail.com>
2023-01-23 18:15:25 -0700

    Moified hello.py to print the query parameters

--- hello.py
+++ hello.py
@@ -3,8 +3,10 @@
 import os
 import json
 
-print('Content-Type: application/json')
+# print('Content-Type: application/json')
+print('Content-Type: text/html')
 print()
-j = json.dumps(dict(os.environ), indent=2)
+# j = json.dumps(dict(os.environ), indent=2)
+print(f"QUERY_STRING = {os.environ['QUERY_STRING']}")
 
-print(j)
+# print(j)
